d93a2fff4af8c2689159c850e2be6d48364d6deb
Kent Pedersen <webzone@kpdesign.dk>
2021-05-09 21:08:53 +0200

    added up, down, and automatic buttons

--- SmartCurtain/MainWindow.xaml.cs
+++ SmartCurtain/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
 
 namespace SmartCurtain
 {
@@ -25,94 +27,89 @@ namespace SmartCurtain
 
         protected void PortCreater()
         {
-            myPort = new SerialPort();
-            myPort.BaudRate = 9600;
-            myPort.PortName = "COM5";
+            myPort = new SerialPort("COM5", 9600);
+            //myPort.BaudRate = 9600;
+            //myPort.PortName = "COM5";
         }
-
         public MainWindow()
         {
             InitializeComponent();
+            PortCreater();
+          
         }
 
         private void ButtonClickPreset0Percent_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-            myPort.WriteLine("0");
+            myPort.Write("0");
             myPort.Close();
         }
 
         private void ButtonClickPreset25Percent_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-            myPort.WriteLine("25");
+            myPort.Write("1");
             myPort.Close();
         }
 
         private void ButtonClickPreset50Percent_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
+           
             myPort.Open();
             //Here we can write to the arduino
-            myPort.WriteLine("50");
+            myPort.Write("2");
             myPort.Close();
         }
 
         private void ButtonClickPreset75Percent_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-            myPort.WriteLine("75");
+            myPort.Write("3");
             myPort.Close();
         }
 
         private void ButtonClickPreset100Percent_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-            myPort.WriteLine("100");
+            myPort.Write("4");
             myPort.Close();
         }
 
         private void ButtonClickTurnAutomationOn_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-
+            myPort.Write("a");
             myPort.Close();
         }
 
         private void ButtonClickTurnAutomationOff_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
+            myPort.Write("m");
 
             myPort.Close();
         }
 
         private void ButtonClickManuelDown_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
+            myPort.Write("d");
 
             myPort.Close();
         }
 
         private void ButtonClickManuelUp_Click(object sender, RoutedEventArgs e)
         {
-            PortCreater();
             myPort.Open();
             //Here we can write to the arduino
-
+            myPort.Write("u");
             myPort.Close();
         }
     }
